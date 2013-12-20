6a6f330915f738239b69b9a687a717fb33eb3042
Joshua Rahm <joshua.rahm@colorado.edu>
2013-12-20 00:04:54 -0700

    broken stuff; revert back

--- Display.hs
+++ Display.hs
@@ -1,7 +1,7 @@
 module Display where
 
 import Graphics.UI.GLUT
-import Debug.Trace
+-- import Debug.Trace
 import Data.IORef
 import Cube
 
@@ -14,29 +14,11 @@ data Camera = Camera {
 
 cameraLookAt :: Camera -> IO()
 cameraLookAt (Camera pos look up) = do
-    perspective 50 1.8 0.1 200
-    lookAt pos look up
+    perspective 50 1.8 0.2 200
+    lookAt (Vertex3 0 0 1) (Vertex3 0 0 0) (Vector3 0 0 1)
 
-plot :: PointColor -> IO ()
-plot (PointColor vert col) = do
-    vertex $ vert
-    color  $ col
-
-display :: (IORef GLfloat, IORef Camera) -> DisplayCallback
-display (angle, camera) = do
-    let strip rows cols  = if rows <= 0
-        then []
-        else [ PointColor (Vertex3 x y 0) (Color3 (0.2 * x) (0.2 * y) 1) | x <- [0..cols], y <- [rows-1,rows] ] ++ (strip (rows-1) cols)
-    let temp :: (GLfloat,GLfloat,GLfloat) ; temp = (0,-1,0)
-    let (x,y,z) = temp
-
-    get camera >>= cameraLookAt
-    clear [ColorBuffer]
-    loadIdentity
-    a <- get angle
-    rotate a $ Vector3 0 0 1
-    preservingMatrix $ do
-        translate $ Vector3 x y z
-        renderPrimitive QuadStrip $ do
-            (mapM_ plot (strip 3 10))
-    swapBuffers
+display :: (a,b) -> DisplayCallback
+display (_,_) = do 
+  clear [ColorBuffer]
+  cube 0.2
+  flush
